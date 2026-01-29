59db658b5a55c4ff2860a755bfc56b0ce23daa4f
pedro-gomezm-um-es <pedro.gomezm@um.es>
2026-01-29 10:04:09 +0100

    tablero y comprobacion de movimientos completada

---
+++ src/juego_3_en_raya.py
@@ -0,0 +1,101 @@
+fichas= ['o','x']
+
+def mostrar_tablero(n, movimientos_jugadores):
+
+    for i in range(n):
+        for j in range(n):
+            casilla_vacia = True
+            for k in range(len(movimientos_jugadores)):
+                movimientos_jugador= movimientos_jugadores[k]
+
+                if i in movimientos_jugador:
+                    if j in movimientos_jugador[i]:
+                        print(fichas[k],end='')
+                        casilla_vacia= False
+            if casilla_vacia:
+                print('_ ',end='')
+        print('\n')
+
+if __name__ == "__main__":
+    #Pedimos el tamaño del tablero en que se va a realizar el juego
+    n=int(input('Introduce el tamaño del tablero cuadrado:'))
+
+    casillas_libres = n*n
+    jugador_activo = 0
+
+    movimientos_jugador_1 = {}
+    movimientos_jugador_2 = {}
+    movimientos_jugadores = [movimientos_jugador_1, movimientos_jugador_2]
+
+    mostrar_tablero(n,movimientos_jugadores)
+
+import pytest
+from juego_3_en_raya import mostrar_tablero
+
+@pytest.fixture
+def tablero_dimension():
+    return 3
+
+@pytest.fixture
+def movimientos_ambos_jugadores():
+    return [{}, {}]
+
+def test_mostrar_tablero(tablero_dimension, movimientos_ambos_jugadores, capsys):
+    mostrar_tablero(tablero_dimension, movimientos_ambos_jugadores)
+    captured = capsys.readouterr()
+    lineas = captured.out.strip().split("\n")
+    lineas= [l for l in lineas if l]
+    assert len(lineas) == tablero_dimension
+    for linea in lineas:
+        assert len(linea.replace(' ', '')) == tablero_dimension
+
+def movimiento_valido(n, x, y, movimientos_otro_jugador):
+    if x > n or y > n:
+        return False
+    
+    if x in movimientos_otro_jugador:
+        movimientos_en_columna= movimientos_otro_jugador[x]
+        if y in movimientos_en_columna:
+            return False
+
+    return True
+
+@pytest.fixture
+def movimientos_vacios():
+    return {}, {}
+
+@pytest.fixture
+def movimientos_vacios():
+    return {}, {}
+
+@pytest.fixture
+def movimientos_ocupados():
+    return {2: [3]}
+
+@pytest.fixture
+def movimientos_fuera_tablero(tablero_dimension):
+    return tablero_dimension + 1, tablero_dimension + 1
+
+
+def test_movimiento_columna_fuera_tablero(tablero_dimension, movimientos_vacios):
+    movimientos_otro_jugador, _ = movimientos_vacios
+    x = 1
+    y = tablero_dimension + 1
+    assert not movimiento_valido(tablero_dimension, x, y, movimientos_otro_jugador)
+
+def test_movimiento_fila_fuera_tablero(tablero_dimension, movimientos_vacios):
+    movimientos_otro_jugador, _ = movimientos_vacios
+    x = tablero_dimension + 1
+    y = 1
+    assert not movimiento_valido(tablero_dimension, x, y, movimientos_otro_jugador)
+
+def test_movimiento_fila_y_columna_fuera_tablero(tablero_dimension, movimientos_vacios, movimientos_fuera_tablero):
+    movimientos_otro_jugador, _ = movimientos_vacios
+    x, y = movimientos_fuera_tablero
+    assert not movimiento_valido(tablero_dimension, x, y, movimientos_otro_jugador)
+
+
+def test_movimiento_incorrecto(tablero_dimension, movimientos_ocupados):
+    x = 2
+    y = 3
+    assert not movimiento_valido(tablero_dimension, x, y, movimientos_ocupados)
